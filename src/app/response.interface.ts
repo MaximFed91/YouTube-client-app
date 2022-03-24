64909a11ca619ebca39f6ddbd95ec83ab6c3625a
Maksim <honestliar91@gmail.com>
2022-03-24 22:01:29 +0300

    feat: add response interface

--- src/app/response.interface.ts
+++ src/app/response.interface.ts
@@ -1,3 +1,50 @@
-export interface Response {
+export interface IThumbnailsSize {
+  url: string;
+  width: number;
+  height: number;
 }
 
+export interface IResponseItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: IThumbnailsSize;
+      medium: IThumbnailsSize;
+      high: IThumbnailsSize;
+      standard: IThumbnailsSize;
+      maxres: IThumbnailsSize;
+    };
+    channelTitle: string;
+    tags: string[];
+    categoryId: string;
+    liveBroadcastContent: string;
+    localized: {
+      title: string;
+      description: string;
+    };
+    defaultAudioLanguage: string;
+  };
+  statistics: {
+    viewCount: string;
+    likeCount: string;
+    dislikeCount: string;
+    favoriteCount: string;
+    commentCount: string;
+  };
+}
+
+export interface Response {
+  kind: string;
+  etag: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: IResponseItem[];
+}
