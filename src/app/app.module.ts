e7e5ffe31ab09bda45ad15553f67ae4aa855bc3c
Maksim <honestliar91@gmail.com>
2022-03-25 14:35:53 +0300

    fest: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
   ],
   imports: [
     BrowserModule,
