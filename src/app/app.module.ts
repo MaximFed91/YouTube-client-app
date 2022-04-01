4fc712066db13e7be5f53da36f5f52f4d6387a71
Maksim <honestliar91@gmail.com>
2022-04-01 18:23:15 +0300

    feat: add basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchResultsComponent } from './search/search-results/search-results.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
+import { LoginBlockComponent } from './header/login-block/login-block.component';
+import { FilterBlockComponent } from './filter-block/filter-block.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchResultsComponent, SearchItemComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    LoginBlockComponent,
+    FilterBlockComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/filter-block/filter-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-block',
+  templateUrl: './filter-block.component.html',
+  styleUrls: ['./filter-block.component.scss']
+})
+export class FilterBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
