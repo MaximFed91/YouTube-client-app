e63a9dd2be1458ce6fb8c89d67c2826ef1551efb
Maksim <honestliar91@gmail.com>
2022-04-14 16:52:27 +0300

    feat: add validations message

--- src/app/core/pages/admin/admin.component.ts
+++ src/app/core/pages/admin/admin.component.ts
@@ -7,22 +7,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
+  videoRegexp = new RegExp('https:\/\/(www\.)*youtube\.com\/watch\?v=[A-z0-9]+');
   cardForm = new FormGroup({
-    title: new FormControl('', [Validators.required]),
+    title: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+      Validators.minLength(3),
+    ]),
     description: new FormControl('', Validators.maxLength(255)),
-    img: new FormControl(''),
-    video: new FormControl(''),
+    img: new FormControl('', [
+      Validators.required,
+      Validators.pattern('https://i.ytimg.com/vi/[A-z0-9/]*.jpg'),
+    ]),
+    video: new FormControl('', [Validators.required, Validators.pattern(this.videoRegexp)]),
   });
   submit() {
     console.log('submit');
-  }
-  getMessageTitle() {
-    return this.cardForm.get('title')?.errors;
-  }
-  getMessageImg() {
-    console.log(this.cardForm.get('img')?.errors);
-  }
-  getMessageVideo() {
-    console.log(this.cardForm.get('video')?.errors);
   }
 }
